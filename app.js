c8399607bbab8457d1855fc6edcc624129c3678e
Nicolas <nicolas.grissi@gmail.com>
2020-05-28 15:48:34 +0200

    put two elements buttons listener in one on body and filter by target

--- app.js
+++ app.js
@@ -32,32 +32,6 @@ class Item {
   }
 }
 
-let items = [];
-const itemAddInput = document.getElementById("itemAddInput");
-const itemAddButton = document.getElementById("itemAddButton");
-const ItemsAdded = document.getElementById("itemsAdded");
-
-itemAddInput.value = "";
-
-itemAddButton.addEventListener("click", function () {
-  if (itemAddInput.value !== "") {
-    items.push(new Item(itemAddInput.value, props));
-    itemAddInput.value = "";
-    let allItems = "";
-    items.map((item) => {
-      allItems += `
-      <li class="list-group-item">
-        <span class="items">${item.content} </span>
-        <div class="ml-5 d-inline form-group form-check" >
-          <input type="checkbox" class="form-check-input" id="${item.content}" />
-          <label class="form-check-label" for="${item.content}"><small>?</small></label>
-        </div>
-      </li>`;
-    });
-    ItemsAdded.innerHTML = allItems;
-  } else return;
-});
-
 /**
  * Represent a property to compare
  * @constructor
@@ -100,22 +74,108 @@ class Prop {
   }
 }
 
+let items = [];
+const itemAddInput = document.getElementById("itemAddInput");
+const itemAddButton = document.getElementById("itemAddButton");
+const ItemsAdded = document.getElementById("itemsAdded");
+
 let props = [];
 const propAddInput = document.getElementById("propAddInput");
 const propAddButton = document.getElementById("propAddButton");
 const propAdded = document.getElementById("propAdded");
 
+// initialize inputs to 0
+itemAddInput.value = "";
 propAddInput.value = "";
 
-propAddButton.addEventListener("click", function () {
-  if (propAddInput.value !== "") {
-    props.push(new Prop(propAddInput.value));
-    propAddInput.value = "";
-    let allProps = "";
-    props.map((prop) => {
-      allProps += `
+document.body.addEventListener("click", function (e) {
+  if (e.target === itemAddButton) {
+    if (itemAddInput.value !== "") {
+      items.push(new Item(itemAddInput.value, props));
+      itemAddInput.value = "";
+      let allItems = "";
+      items.map((item) => {
+        allItems += `
+      <li class="list-group-item">
+        <span class="items">${item.content} </span>
+        <div class="ml-5 d-inline form-group form-check" >
+          <input type="checkbox" class="form-check-input" id="${item.content}" />
+          <label class="form-check-label" for="${item.content}"><small>?</small></label>
+        </div>
+      </li>`;
+      });
+      ItemsAdded.innerHTML = allItems;
+    } else return;
+  } else if (e.target === propAddButton) {
+    if (propAddInput.value !== "") {
+      props.push(new Prop(propAddInput.value));
+      propAddInput.value = "";
+      let allProps = "";
+      props.map((prop) => {
+        allProps += `
       <li class="list-group-item">${prop.content}</li>`;
-    });
-    propAdded.innerHTML = allProps;
+      });
+      propAdded.innerHTML = allProps;
+    } else return;
   } else return;
 });
+
+document.addEventListener("change", function () {
+  let blockCompare = document.getElementById("blockCompare");
+
+  let allCompareProps = "";
+  props.map((prop) => {
+    allCompareProps += `
+      <li class="list-group-item">
+                <div class="row">
+                  <div class="col-md-8">${prop.content}</div>
+                  <div class="col-md-4">
+                    <div
+                      class="btn-group btn-group-toggle"
+                      data-toggle="buttons"
+                    >
+                      <label class="btn btn-sm btn-secondary">
+                        <input
+                          type="radio"
+                          name="options"
+                          id="option1"
+                          checked
+                        />
+                        Active
+                      </label>
+                      <label class="btn btn-sm btn-secondary">
+                        <input type="radio" name="options" id="option2" />
+                        Radio
+                      </label>
+                      <label class="btn btn-sm btn-secondary">
+                        <input type="radio" name="options" id="option3" />
+                        Radio
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </li>`;
+  });
+  let allCompareItems = "";
+
+  items.map((item) => {
+    allCompareItems += `
+    
+          <div class="card mb-3">
+            <div class="card-header">
+              <h5 id="allCompareTitle" class="card-title">${item.content}</h5>
+            </div>
+            <ul id="allCompareList" class="list-group list-group-flush">
+            ${allCompareProps}
+              
+            </ul>
+          </div>
+        
+    `;
+  });
+
+  blockCompare.innerHTML = allCompareItems;
+});
+
+let allCompareList = document.getElementById("allCompareList");
+let allCompareTitle = document.getElementById("allCompareTitle");
