d26b1de2fb35f5b41bd6e7e64f898d149acadf95
Nicolas <nicolas.grissi@gmail.com>
2020-05-31 20:45:15 +0200

    update getId() function logic with date

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ class Item {
    * @return {int} the id
    */
   static getId() {
-    return Item.inc++;
+    return Date.now();
   }
+
   randColor() {
     const letters = "0123456789ABCDEF";
     let color = "#";
@@ -99,7 +100,7 @@ document.body.addEventListener("click", function (e) {
       <li class="list-group-item">
         <span class="items">${item.content} </span>
         <div class="ml-5 d-inline form-group form-check" >
-          <input type="checkbox" class="form-check-input" id="${item.content}" />
+          <input type="checkbox" data-id="${item.id}" class="form-check-input" id="${item.content}" />
           <label class="form-check-label" for="${item.content}"><small>?</small></label>
         </div>
       </li>`;
@@ -119,6 +120,9 @@ document.body.addEventListener("click", function (e) {
       propAdded.innerHTML = allProps;
       updateCompare();
     } else return;
+  }
+  if (e.target === rankList) {
+    console.log(rankList);
   } else return;
 });
 
@@ -127,31 +131,11 @@ function updateCompare() {
   let allCompareProps = "";
   props.map((prop) => {
     allCompareProps += `
-      <li class="list-group-item">
+      <li class="list-group-item ">
                 <div class="row">
                   <div class="col-md-8">${prop.content}</div>
                   <div class="col-md-4">
-                    <div
-                      class="btn-group btn-group-toggle"
-                      data-toggle="buttons"
-                    >
-                      <label class="btn btn-sm btn-secondary">
-                        <input
-                          type="radio"
-                          name="options"
-                          id="option1"
-                        />
-                        20%
-                      </label>
-                      <label class="btn btn-sm btn-secondary">
-                        <input type="radio" name="options" id="option2" />
-                        70%
-                      </label>
-                      <label class="btn btn-sm btn-secondary">
-                        <input type="radio" name="options" id="option3" />
-                        100%
-                      </label>
-                    </div>
+                    <input type="range" min="0" max="3" class="form-control-range rankList">
                   </div>
                 </div>
               </li>`;
@@ -163,9 +147,9 @@ function updateCompare() {
     
           <div class="card mb-3">
             <div class="card-header">
-              <h5 id="allCompareTitle" class="card-title">${item.content}</h5>
+              <h5 id="allCompareTitle" data-id="${item.id}" class="card-title">${item.content}</h5>
             </div>
-            <ul id="allCompareList" class="list-group list-group-flush">
+            <ul class="allCompareUl list-group list-group-flush">
             ${allCompareProps}
               
             </ul>
@@ -176,3 +160,15 @@ function updateCompare() {
 
   blockCompare.innerHTML = allCompareItems;
 }
+
+let rankList = document.getElementsByClassName("rankList");
+
+document.body.addEventListener("change", function (e) {
+  for (let i = 0; i < rankList.length; i++) {
+    items.forEach(function (item) {
+      for (let j = 0; j < props.length; j++) {
+        item.props[j].fit = rankList[i].value;
+      }
+    });
+  }
+});
