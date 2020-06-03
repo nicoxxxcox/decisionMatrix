e1d0688d9db99d5601ff46faabb574c9ecb1f331
Nicolas <nicolas.grissi@gmail.com>
2020-06-03 12:34:54 +0200

    implement views

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
 /**
  * Represent an item to compare
  * @constructor
- * @param {string} content - content
- * @param {object} param - property to compare
+ * @param {string} title - title of the itel
  */
 class Item {
-  constructor(content, props) {
-    this.id = Item.getId();
-    this.content = content;
+  constructor(title) {
+    this.id = this.getId();
+    this.title = title;
     this.color = this.randColor();
     this.isSure = true;
-    this.props = props;
   }
 
   /**
    * Generate number id
    * @return {int} the id
    */
-  static getId() {
-    return Date.now();
+  getId() {
+    return `item-${Date.now()}`;
   }
 
   randColor() {
@@ -29,17 +27,69 @@ class Item {
     }
     return color;
   }
+
+  /**
+   *
+   * @param {object} props
+   */
+  setProp(props) {
+    this.props = props;
+  }
+
+  /**
+   * @returns {string}
+   */
+  addView() {
+    return `
+    <li class="list-group-item">
+      <span class="items">${this.title} </span>
+      <div class="ml-5 d-inline form-group form-check" >
+        <input type="checkbox" data-id="item-${this.id}" class="form-check-input"/>
+        <label class="form-check-label" for="${this.title}"><small>?</small></label>
+      </div>
+    </li>
+    `;
+  }
+
+  /**
+   * @returns {string}
+   */
+  compareView() {
+    return `
+    <div class="card mb-3">
+      <div class="card-body">
+        <h5 id="allCompareTitle" data-id="item-${this.id}" class="card-title">${
+      this.title
+    }</h5>
+      </div>
+      <ul class="allCompareUl list-group list-group-flush">
+        ${
+          this.props
+            ? this.props
+                .map(
+                  (prop) => `<li class="list-group-item d-flex">
+        <div class="mr-auto">${prop.title}</div>
+        <span class="result badge badge-primary">0</span><input type="range" min="0" max="3" value="0" class="form-control-range w-25 rankList">
+      </li>`
+                )
+                .join("")
+            : ""
+        }
+      </ul>
+    </div>
+    `;
+  }
 }
 
 /**
  * Represent a property to compare
  * @constructor
- * @param {string} content - content
+ * @param {string} title - title of prop
  */
 class Prop {
-  constructor(content) {
-    this.id = Prop.getId();
-    this.content = content;
+  constructor(title) {
+    this.id = this.getId();
+    this.title = title;
     this.color = this.randColor();
     this.isSure = true;
     this.fit = 0;
@@ -49,8 +99,8 @@ class Prop {
    * Generate number id
    * @return {int} the id
    */
-  static getId() {
-    return Date.now();
+  getId() {
+    return `prop-${Date.now()}`;
   }
 
   setFit(fit) {
@@ -69,97 +119,109 @@ class Prop {
     }
     return color;
   }
+
+  addView() {
+    return `<li class="list-group-item">${this.title}</li>`;
+  }
 }
 
-let main = function () {
-  let items = [];
-  const itemAddInput = document.getElementById("itemAddInput");
-  const itemAddButton = document.getElementById("itemAddButton");
-  const ItemsAdded = document.getElementById("itemsAdded");
+// main program begin here
+// -----------------------
+const blockCompare = document.getElementById("js-blockCompare");
 
-  let props = [];
-  const propAddInput = document.getElementById("propAddInput");
-  const propAddButton = document.getElementById("propAddButton");
-  const propAdded = document.getElementById("propAdded");
+let items = [];
+const itemAddInput = document.getElementById("js-itemAddInput");
+const itemAddBtn = document.getElementById("js-itemAddBtn");
+const itemAdd = document.getElementById("js-itemAdd");
 
-  // initialize inputs to 0
+// init value to ""
+itemAddInput.value = "";
+
+itemAddBtn.addEventListener("click", function () {
+  // new instance of item with input value
+  let item = new Item(itemAddInput.value);
+  // add to items array
+  items.push(item);
+
+  updateProps();
+  // insert in html
+  itemAdd.insertAdjacentHTML("beforeend", item.addView(item));
+  renderCompare();
+  // put input to ""
   itemAddInput.value = "";
+});
+
+let props = [];
+const propAddInput = document.getElementById("js-propAddInput");
+const propAddBtn = document.getElementById("js-propAddBtn");
+const propAdd = document.getElementById("js-propAdd");
+
+propAddInput.value = "";
+
+propAddBtn.addEventListener("click", function () {
+  let prop = new Prop(propAddInput.value);
+  props.push(prop);
+  updateProps();
+
+  propAdd.insertAdjacentHTML("beforeend", prop.addView(prop));
+
+  renderCompare();
   propAddInput.value = "";
+});
 
-  document.body.addEventListener("click", function (e) {
-    if (e.target === itemAddButton) {
-      if (itemAddInput.value !== "") {
-        let item = new Item(itemAddInput.value, props);
-        items.push(item);
-        ItemsAdded.insertAdjacentHTML(
-          "beforeend",
-          `
-        <li class="list-group-item">
-          <span class="items">${item.content} </span>
-          <div class="ml-5 d-inline form-group form-check" >
-            <input type="checkbox" data-id="${item.id}" class="form-check-input" id="${item.content}" />
-            <label class="form-check-label" for="${item.content}"><small>?</small></label>
-          </div>
-        </li>
-        `
-        );
-        itemAddInput.value = "";
-        updateCompare();
-      } else return;
-    } else if (e.target === propAddButton) {
-      if (propAddInput.value !== "") {
-        let prop = new Prop(propAddInput.value);
-        props.push(prop);
-        propAdded.insertAdjacentHTML(
-          "beforeend",
-          `
-        <li class="list-group-item">${prop.content}</li>
-        `
-        );
-        propAddInput.value = "";
-        updateCompare();
-      } else return;
-    } else return;
+function updateProps() {
+  items.forEach(function (item) {
+    item.setProp(props);
   });
+}
 
-  function updateCompare() {
-    let blockCompare = document.getElementById("blockCompare");
-    let allCompareProps = "";
-    props.map((prop) => {
-      allCompareProps += `
-      <li class="list-group-item d-flex">
-        <div class="mr-auto">${prop.content}</div>
-        <span class="result badge badge-primary">0</span><input type="range" min="0" max="3" value="0" class="form-control-range w-25 rankList">
-      </li>
-      `;
-    });
-    let allCompareItems = "";
+function renderCompare() {
+  let render = "";
+  items.map(function (item) {
+    render += item.compareView();
+  });
+  blockCompare.innerHTML = render;
+}
 
-    items.map((item) => {
-      allCompareItems += `
-    
-          <div class="card mb-3">
-          <div class="card-body"><h5 id="allCompareTitle" data-id="${item.id}" class="card-title">${item.content}</h5></div>
-            
-            <ul class="allCompareUl list-group list-group-flush">
-            ${allCompareProps}
-              
-            </ul>
-          </div>
-        
+/*
+document.body.addEventListener("click", function (e) {
+  if (e.target === itemAddBtn) {
+    if (itemAddInput.value !== "") {
+      let item = new ItemController(
+        new ItemModel(itemAddInput.value),
+        new ItemView()
+      );
+      items.push(item);
+      itemAdd.insertAdjacentHTML("beforeend", item.itemView.AddView(item));
+      itemAddInput.value = "";
+      updateCompare();
+    } else return;
+  } else if (e.target === propAddBtn) {
+    if (propAddInput.value !== "") {
+      let prop = new Prop(propAddInput.value);
+      props.push(prop);
+      propAdded.insertAdjacentHTML(
+        "beforeend",
+        `
+        <li class="list-group-item">${prop.title}</li>
+        `
+      );
+      propAddInput.value = "";
+      updateCompare();
+    } else return;
+  } else return;
+});
+
+
+function updateCompare() {
+  const blockCompare = document.getElementById("js-blockCompare");
+  let allCompareProps = "";
+  props.map((prop) => {
+    allCompareProps += `
+    <li class="list-group-item d-flex">
+    <div class="mr-auto">${prop.title}</div>
+    <span class="result badge badge-primary">0</span><input type="range" min="0" max="3" value="0" class="form-control-range w-25 rankList">
+    </li>
     `;
-    });
-
-    blockCompare.innerHTML = allCompareItems;
-  }
-
-  const allCompareUl = document.getElementsByClassName("allCompareUl");
-  for (const el of allCompareUl) {
-    el.addEventListener("change", function (e) {
-      const result = document.querySelector(".result");
-      result.textContent = e.target.value;
-    });
-  }
-};
-
-main();
+  });
+  */
