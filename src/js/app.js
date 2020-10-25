5ceb178f1383f7cfe4eca9e0e5694efd8f1313fc
Nicolas <nicolas.grissi@gmail.com>
2020-10-25 09:53:04 +0100

    add eslint and .editorconfig configs

--- src/js/app.js
+++ src/js/app.js
@@ -6,28 +6,28 @@ import { Controler } from "./controler.js";
  * @param {string} el - wrapper DOM element where the table where build
  */
 class Table {
-  constructor(el = "app") {
-    if (typeof el !== "string") {
-      this.el = "app";
-      console.log("the app will build in #app div");
-    } else {
-      this.el = el;
+    constructor(el = "app") {
+        if (typeof el !== "string") {
+            this.el = "app";
+            console.log("the app will build in #app div");
+        } else {
+            this.el = el;
+        }
+
+
+        this.main();
+    
     }
 
+    main() {
+        this.wrapper = document.getElementById(this.el);
 
-    this.main();
-    
-  }
-
-  main() {
-    this.wrapper = document.getElementById(this.el);
-
-    this.controler = new Controler(this.wrapper);
-    this.controler.initRender(this.wrapper);
+        this.controler = new Controler(this.wrapper);
+        this.controler.initRender(this.wrapper);
     
 
-    this.controler.listenClickEvents(this.wrapper);
-  }
+        this.controler.listenClickEvents(this.wrapper);
+    }
 
 }
 
